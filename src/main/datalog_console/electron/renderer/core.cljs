aeb2d750c596437f2fcccdb421fdc472e2e0df75
groundedSAGE <wade@lambdaforge.io>
2021-09-10 20:29:50 +0200

    Add UI

--- src/main/datalog_console/electron/renderer/core.cljs
+++ src/main/datalog_console/electron/renderer/core.cljs
@@ -1,22 +1,9 @@
 (ns datalog-console.electron.renderer.core
-  (:require [reagent.core :refer [atom]]
-            [reagent.dom :as rd]))
+  (:require [reagent.dom :as r]
+            [datalog-console.client]))
 
-(enable-console-print!)
-
-(defonce state (atom 0))
-
-(defn root-component []
-  [:div
-   [:div.logos
-    [:img.electron {:src "img/electron-logo.png"}]
-    [:img.cljs {:src "img/cljs-logo.svg"}]
-    [:img.reagent {:src "img/reagent-logo.png"}]]
-   [:button
-    {:on-click #(swap! state inc)}
-    (str "Clicked " @state " times")]])
 
 (defn ^:dev/after-load start! []
-  (rd/render
-   [root-component]
+  (r/render
+   [datalog-console.client/root]
    (js/document.getElementById "root")))
